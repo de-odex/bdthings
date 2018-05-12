b4a2fdfec8a3a9d2fa14fdc65c4e00d8b22b4659
Justin Kyle Ramos <justinfblover@gmail.com>
2018-05-12 17:43:42 +0800

    change to jsencrypt; clean bloat; finish decrypt command and added constant encrypt toggle

--- discord crypt/discypher.plugin.js
+++ discord crypt/discypher.plugin.js
@@ -1,4 +1,4 @@
-//META{"name":"discypher", "source": "https://cdn.rawgit.com/de-odex/bdthings/4f936dde/discord%20crypt/discypher.plugin.js"}*//
+//META{"name":"discypher", "source": "https://github.com/de-odex/bdthings/blob/master/discord%20crypt/discypher.plugin.js"}*//
 
 var CustomSettings = {}
 CustomSettings.TextArea = class TextArea {
@@ -57,16 +57,21 @@ Array.prototype.remove = function(from, to) {
   return this.push.apply(this, rest);
 };
 
+String.prototype.replaceBetween = function(start, end, what) {
+    return this.substring(0, start) + what + this.substring(end);
+};
+
 class discypher {
     getName() {return "discypher";}
     getDescription() {return "Automatically decrypt and encrypt messages via RSA";}
     getVersion() {return "0.1.0";} // angery! give me my numerical versions! xd
     getWordVersion() {return "one";} // not required, i just prefer it.
     getAuthor() {return "de/odex";}
+    getSource() {return "https://cdn.rawgit.com/de-odex/bdthings/4f936dde/discord%20crypt/discypher.plugin.js"}
 
     //__init__: tales of a python programmer
     constructor(){
-        this.default_settings = {priv_key: null, pub_key: {}, encrypt: false, decrypt: false}
+        this.default_settings = {priv_key: null, pub_key: {}, encrypt: {}, decrypt: false}
         this.settings = this.default_settings  // almost typed None there
         this.modalHTML = `<div id="\${id}" class="theme-dark">
                             <div class="backdrop backdrop-1ocfXc" style="background-color: rgb(0, 0, 0); opacity: 0.85;"></div>
@@ -86,10 +91,7 @@ class discypher {
                                     </div>
                                 </div>
                             </div>
-                        </div>`;
-
-        this.msg_parse = InternalUtilities.WebpackModules.findByUniqueProperties(["createBotMessage"])
-        this.msg_send = InternalUtilities.WebpackModules.findByUniqueProperties(["sendClydeError"])
+                        </div>`;   
     }
 
 
@@ -108,26 +110,14 @@ class discypher {
             document.head.appendChild(libraryScript);
         }
 
+
         // this is me being lazy
         {
-            let jsbn = document.createElement("script");
-            jsbn.setAttribute("type", "text/javascript");
-            jsbn.setAttribute("src", "https://cdn.rawgit.com/de-odex/bdthings/518d7e2e/discord%20crypt/deps/jsbn.js");
-            jsbn.setAttribute("id", "jsbnLib");
-            document.head.appendChild(jsbn);
-
-            let rsa = document.createElement("script");
-            rsa.setAttribute("type", "text/javascript");
-            rsa.setAttribute("src", "https://cdn.rawgit.com/de-odex/bdthings/518d7e2e/discord%20crypt/deps/rsa.js");
-            rsa.setAttribute("id", "rsaLib");
-            document.head.appendChild(rsa);
-
-            let rsa2 = document.createElement("script");
-            rsa.setAttribute("type", "text/javascript");
-            rsa.setAttribute("src", "https://cdn.rawgit.com/de-odex/bdthings/518d7e2e/discord%20crypt/deps/rsa2.js");
-            rsa.setAttribute("id", "rsa2Lib");
-            document.head.appendChild(rsa2);
-        
+            let jsenc = document.createElement("script");
+            jsenc.setAttribute("type", "text/javascript");
+            jsenc.setAttribute("src", "https://cdn.rawgit.com/travist/jsencrypt/66792978/bin/jsencrypt.js");
+            jsenc.setAttribute("id", "jsencLib");
+            document.head.appendChild(jsenc);
         }
 
         if (window.ZeresLibrary) this.initialize()
@@ -146,53 +136,159 @@ class discypher {
     observer(e) {
         if (!e.addedNodes.length || !(e.addedNodes[0] instanceof Element) || !this.has_init) return;
 
+        var elem = e.addedNodes[0];
+
         if (e.addedNodes[0].classList && this.has_init) {
-            var elem = e.addedNodes[0]
+            let channel = ReactUtilities.getOwnerInstance($("form")[0])
+            if (!channel) return
+            channel = channel.props.channel
+            if (channel.type != 1) return
+
             var contextmenu = elem.classList.contains(DiscordClasses.ContextMenu.contextMenu) ? elem : elem.querySelector(DiscordSelectors.ContextMenu.contextMenu)
-            if (!contextmenu || !(contextmenu.parentNode.id == "app-mount")) return
-            if (!(ReactUtilities.getReactProperty(contextmenu, "return.memoizedProps.type") == DiscordModules.DiscordConstants.ContextMenuTypes.USER_CHANNEL_TITLE) &&
-                !(ReactUtilities.getReactProperty(contextmenu, "return.memoizedProps.type") == DiscordModules.DiscordConstants.ContextMenuTypes.USER_PRIVATE_CHANNELS) &&
-                !(ReactUtilities.getReactProperty(contextmenu, "return.memoizedProps.type") == DiscordModules.DiscordConstants.ContextMenuTypes.USER_PRIVATE_CHANNELS_MESSAGE)) return
+            if ((contextmenu) && (contextmenu.parentNode.id == "app-mount")) {
+                let type = ReactUtilities.getReactProperty(contextmenu, "return.memoizedProps.type")
+                Logger.debug("", type)
+                if ((type == DiscordModules.DiscordConstants.ContextMenuTypes.USER_CHANNEL_TITLE) ||
+                    (type == DiscordModules.DiscordConstants.ContextMenuTypes.USER_PRIVATE_CHANNELS) ||
+                    (type == DiscordModules.DiscordConstants.ContextMenuTypes.USER_PRIVATE_CHANNELS_MESSAGE)) {
 
-            let user = ReactUtilities.getReactProperty(contextmenu, "return.stateNode.props.user")
-            if (user.bot) return
-            Logger.debug("", user)
+                    let user = ReactUtilities.getReactProperty(contextmenu, "return.stateNode.props.user")
+                    if (user.bot) return
+                    //new submenu
+                    let subMenu = new PluginContextMenu.SubMenuItem("discypher", new PluginContextMenu.Menu().addItems(
+                        new PluginContextMenu.TextItem("public key", {callback: () => {
+                            let modal = $(PluginUtilities.formatString(this.modalHTML, {modalTitle: "public key", id: "pub-key"}))
+                            var panel = $("<form>").addClass("form").css("width", "100%")
+                            var pktextarea = new CustomSettings.TextArea("public key", "the user's public key. i'm no stylist so forgive the bad format", 
+                                {type: "text", placeholder: "-----BEGIN PUBLIC KEY-----...", value: this.settings.pub_key[user.id], rows: 10, cols: 64}, 
+                                (val) => { this.settings.pub_key[user.id] = val; this.set_settings() })
+                            panel.append(pktextarea.getElement())
+                            modal.find(".selectable").append(panel)
+                            modal.find(".backdrop, .close-button, .done-button").on("click", () => {modal.remove()})
+                            modal.appendTo("#app-mount")
+                        }})
+                    ));
+                    let pub_key_grp = new PluginContextMenu.ItemGroup().addItems(subMenu);
+                    $(contextmenu).append(pub_key_grp.getElement()); 
+                } else if (type == DiscordModules.DiscordConstants.ContextMenuTypes.NATIVE_INPUT) {
+                    let user_encrypt = new PluginContextMenu.ItemGroup().addItems(new PluginContextMenu.ToggleItem("encrypt", this.settings.encrypt[channel.recipients[0]], {onChange: cstate => {this.settings.encrypt[channel.recipients[0]] = cstate; this.set_settings()}}));
+                    $(contextmenu).append(user_encrypt.getElement()); 
+                }
+            }
 
+            var textarea = elem.querySelector(DiscordSelectors.Textarea.textArea);
+            if (textarea) {
+                this.textarea_patch($(textarea));
+            }
 
-            //new submenu
-            let subMenu = new PluginContextMenu.SubMenuItem("discypher", new PluginContextMenu.Menu().addItems(
-                new PluginContextMenu.TextItem("public key", {callback: () => {
-                    let modal = $(PluginUtilities.formatString(this.modalHTML, {modalTitle: "public key", id: "pub-key"}))
-                    var panel = $("<form>").addClass("form").css("width", "100%")
-                    var pktextarea = new CustomSettings.TextArea("public key", "the user's public key. i'm no stylist so forgive the bad format", 
-                        {type: "text", placeholder: "-----BEGIN RSA PUBLIC KEY-----...", value: this.settings.pub_key[user.id], rows: 10, cols: 64}, 
-                        (val) => { this.settings.pub_key[user.id] = val; this.set_settings() })
-                    Logger.debug("", pktextarea)
-                    panel.append(pktextarea.getElement())
-                    modal.find(".selectable").append(panel)
-                    modal.find(".backdrop, .close-button, .done-button").on("click", () => {modal.remove()})
-                    modal.appendTo("#app-mount")
-                }})
-            ));
-            let testGroup = new PluginContextMenu.ItemGroup().addItems(subMenu);
-            $(contextmenu).append(testGroup.getElement()); 
-            
+            e.addedNodes.forEach(element => {
+                if (element && element.tagName && element.classList && element.classList.contains("message-group")) {
+                    element.querySelectorAll(".message").forEach(message => {this.messageDecrypt(message);});
+                    Logger.debug("", "msggrp")
+                }
+                else if (element && element.tagName && element.classList && element.classList.contains("message")) {
+                    this.messageDecrypt(element)
+                    Logger.debug("", "msg")
+                }
+            })
+
+            //var messages = $.map(e.addedNodes, function(value, index) {return [value]}).filter(e => {return e.classList.contains("messages-wrapper") || e.classList.contains("message-group")})
+            //if (messages[0]){
+            //    Logger.debug("messages", messages)
+            //    this.messageDecrypt(messages)
+            //}
         }
+    }
 
+    textarea_patch(textarea) {
+        textarea.on("keypress." + this.getName(), (e) => {this.sendEncrypt(e);})
     }
 
     sendEncrypt(e) {
-        let dm = ReactUtilities.getOwnerInstance($("form")[0]).props.channel
-        if (dm.type != 1) return
-        let msg = this.msg_parse.parse(dm, $(DiscordSelectors.Textarea.channelTextArea.toString() + ' textarea').val()).content
-        if (!msg.contains("```encrypt\n")) return
-        let start = msg.indexOf("```encrypt\n")
-        let end = msg.indexOf("```", start + 3)
+        if (e.shiftKey || e.which != 13) return
+        let channel = ReactUtilities.getOwnerInstance($("form")[0]).props.channel
+        let msg = DiscordModules.MessageParser.parse(channel, $(e.currentTarget).val()).content
 
-        ReactUtilities.getOwnerInstance($('form')[0]).setState({textValue: ''});
-        e.preventDefault();
-        e.stopPropagation();
-        return;
+        let start_string = "e{"
+        let end_string = "}"
+
+        let start = msg.indexOf(start_string)
+        let content_start = start != -1 ? start + start_string.length : -1
+        let content_end = msg.indexOf(end_string, start + start_string.length)
+        let end = content_end != -1 ? content_end + end_string.length : -1
+
+        var pub_key = this.settings.pub_key[channel.recipients[0]]
+        if (pub_key === undefined) {PluginUtilities.showToast("no public key given for this user", {type: "error"}); return;}
+        var textarea = $(e.currentTarget)[0]
+
+        if (start!=-1 || end!=-1) {
+            var to_encrypt = msg.substring(content_start, content_end)
+        } else if (this.settings.encrypt[channel.recipients[0]]) {
+            var to_encrypt = msg
+            start = 0
+            end = msg.length
+        } else {return}
+
+        var rsa = new JSEncrypt()
+        rsa.setPublicKey(pub_key)
+        var encrypted = rsa.encrypt(to_encrypt)            
+        textarea.focus();
+        textarea.selectionStart = start;
+        textarea.selectionEnd = end;
+        document.execCommand("insertText", false, "enc:" + channel.recipients[0] + "\n" + encrypted + "\nenc:end");
+    }
+
+    messageDecrypt(messages) {
+        var settings = this.settings
+        if (!settings.decrypt) return
+
+        $(messages).each(function(_, node){
+            $(node).find(".markup").each(function(_, node2){
+                var text = node2.innerText
+                let enc_block = new RegExp("enc:" + DiscordModules.UserInfoStore.getId() + "\n(.*)\nenc:end", "g")
+                var match = enc_block.exec(text)
+
+                if (match && !messages.classList.contains("message-sending")){
+                    if (settings.priv_key === undefined) {PluginUtilities.showToast("no private key given for your account", {type: "error"}); return;}
+                    //start = match.index, end = match.index + match[0].length
+                    Logger.debug("message block", match[1])
+                    Logger.debug("message match", match)
+                    var rsa = new JSEncrypt()
+                    rsa.setPrivateKey(settings.priv_key)
+                    var decrypted = rsa.decrypt(match[1])  // to_decrypt
+                    node2.innerText = text.replaceBetween(match.index, match.index + match[0].length, decrypted)
+                }
+                
+                //node2.innerText = btoa(unescape(encodeURIComponent(node2.innerText)))
+                Logger.debug("message text", (messages.classList.contains("message-sending")?"SENDING: ":"") + node2.innerText)
+            })
+        })
+
+
+        
+        /*
+        let channel = ReactUtilities.getOwnerInstance($("form")[0]).props.channel
+        var message_content = "";
+        var message_node = null;
+        for (var i=0, lnh=messages.length; i < lnh; i++){
+            message_node = $(messages[i]).find(".markup")
+            if (message_node == undefined) return
+            // somewhat broken right now
+            var message_contents = message_node.contents().filter(function(){ return this.nodeType == 3; })
+            Logger.debug("", $(message_contents))
+            for (var j=0, lnh2=message_contents.length; j < lnh2; j++) {
+                let enc_block = new RegExp("enc:" + DiscordModules.UserInfoStore.getId() + "\n(.*)\nenc:end", "g")
+                Logger.debug("", message_contents[j].text())
+                message_contents[j].text("LOL")
+
+
+                //.match(enc_block)
+            }
+            
+            // message_content = message_node.contents().not(message_node.children()).text();
+            //Logger.debug("", message_content)
+        }
+        //*/
     }
 
     getSettingsPanel() {
@@ -211,18 +307,17 @@ class discypher {
 
         var private_key = new CustomSettings.TextArea("private key", "input your private key here (may be unsafe but i'm using it anyway)", {type: "text", placeholder: "-----BEGIN RSA PRIVATE KEY-----...", value: this.settings.priv_key, rows: 10, cols: 64}, (val) => { this.settings.priv_key = val; this.set_settings() })
         private_key.getElement().appendTo(panel)
-        var encrypt = new PluginSettings.Checkbox("encrypt", "automatically encrypt?", this.settings.encrypt, val => { this.settings.encrypt = val; this.set_settings() }, {})
-        encrypt.getElement().appendTo(panel)
         var decrypt = new PluginSettings.Checkbox("decrypt", "automatically decrypt?", this.settings.decrypt, val => { this.settings.decrypt = val; this.set_settings() }, {})
         decrypt.getElement().appendTo(panel)
         $("<p>there is a better way to add public keys in construction. just wait, please</p>").appendTo(panel)
+        $("<p>for now you right click the user name at top of dms</p>").appendTo(panel)
 
         return panel[0];
     }
     
 
     // stolen from zere(tm)
-    update() { PluginUtilities.checkForUpdate(this.getName(), this.getVersion()); }
+    update() { PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), this.getSource()); }
     
 
 
@@ -235,24 +330,21 @@ class discypher {
         this.settings = PluginUtilities.loadSettings(this.getName(), this.default_settings)
     }
 
-
-    // plugin stuff
     init(){
+        this.update()
         this.has_init = true
-
         this.get_settings()
 
+        var textarea = document.querySelector(DiscordSelectors.Textarea.textArea);
+        if (textarea) {
+            this.textarea_patch($(textarea));
+        }
         Logger.info(this.getName(), "version " + this.getWordVersion() + " was initialized")
-
         PluginUtilities.showToast(this.getName() + " " + this.getWordVersion() + " has started.")
     }
 
     deinit(){
         this.has_init = false
-    }
-
-    get_key(){
-
     }
 }
 
